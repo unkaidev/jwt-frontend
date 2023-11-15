a447f885837cbceb03ae551e3bb13062c235bd5c
PhongVan94 <vanbv94@gmail.com>
2023-11-15 16:32:54 +0700

    fix name components

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-
+import "./Users.scss"
 
 const Users = (props) => {
     let history = useHistory();
