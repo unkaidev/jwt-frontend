b70f34dbb0be9340776d85108b68269ff87c7fb4
PhongVan94 <vanbv94@gmail.com>
2023-11-20 10:03:57 +0700

    add delete User, add react-bootrap, modal

--- src/components/ManageUsers/Users.js
+++ src/components/ManageUsers/Users.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import "./Users.scss"
-import { fetchAllUser } from "../../services/userServices";
+import { fetchAllUser, deleteUser } from "../../services/userServices";
 import ReactPaginate from "react-paginate";
+import { toast } from "react-toastify";
+import ModalDelete from "./ModelDelete";
 
 const Users = (props) => {
     const [listUsers, setListUsers] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [currentLimit, setCurrentLimit] = useState(2);
+    const [currentLimit, setCurrentLimit] = useState(3);
     const [totalPages, setTotalPage] = useState(0);
+    const [isShowModalDelete, setIsShowModalDelete] = useState(false);
+    const [dataModal, setDataModel] = useState({});
 
     useEffect(() => {
         fetchUsers();
@@ -25,78 +29,116 @@ const Users = (props) => {
         // await fetchUsers()
     };
 
+    const handleDeleteUser = async (user) => {
+        setDataModel(user);
+        setIsShowModalDelete(true);
+
+    }
+
+    const handleClose = () => {
+        setIsShowModalDelete(false);
+        setDataModel({});
+    }
+    const confirmDeleteUser = async () => {
+        let respone = await deleteUser(dataModal);
+        if (respone && respone.data.EC === 0) {
+            toast.success(respone.data.EM);
+            await fetchUsers();
+            setIsShowModalDelete(false);
+        } else {
+            toast.error(respone.data.EM)
+        }
+    }
+
     return (
-        <div className="container">
+        <>
+            <div className="container">
 
-            <div className="manage-users-container">
-                <div className="user-header">
-                    <div className="title"><h3>Table Users</h3></div>
-                    <div className="actions">
-                        <button className="btn btn-success">Refresh</button>
-                        <button className="btn btn-primary">Add new user</button>
+                <div className="manage-users-container">
+                    <div className="user-header">
+                        <div className="title"><h3>Table Users</h3></div>
+                        <div className="actions">
+                            <button className="btn btn-success">Refresh</button>
+                            <button className="btn btn-primary">Add new user</button>
+                        </div>
                     </div>
-                </div>
-                <div className="user-body">
-                    <table className="table table-bordered table-hover table-striped ">
-                        <thead>
-                            <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">Id</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Group</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {listUsers && listUsers.length > 0 ?
-                                <>
-                                    {listUsers.map((item, index) => {
-                                        return (
-                                            <tr key={`row-${index}`}>
-                                                <td>{index + 1}</td>
-                                                <td>{item.id}</td>
-                                                <td>{item.email}</td>
-                                                <td>{item.username}</td>
-                                                <td>{item.Group ? item.Group.name : ''}</td>
-                                            </tr>
-                                        )
-                                    })}
-                                </>
-                                :
-                                <>
-                                    <tr><td>Not found User</td></tr>
-                                </>}
-                        </tbody>
-                    </table>
+                    <div className="user-body">
+                        <table className="table table-bordered table-hover table-striped ">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Group</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {listUsers && listUsers.length > 0 ?
+                                    <>
+                                        {listUsers.map((item, index) => {
+                                            return (
+                                                <tr key={`row-${index}`}>
+                                                    <td>{index + 1}</td>
+                                                    <td>{item.id}</td>
+                                                    <td>{item.email}</td>
+                                                    <td>{item.username}</td>
+                                                    <td>{item.Group ? item.Group.name : ''}</td>
+                                                    <td>
+                                                        <button className="btn btn-warning mx-3">Edit</button>
+                                                        <button className="btn btn-danger"
+                                                            onClick={() => {
+                                                                handleDeleteUser(item)
+                                                            }}
 
-                </div>
-                {totalPages > 0 &&
-                    <div className="user-footer">
-                        <ReactPaginate
-                            nextLabel="next >"
-                            onPageChange={handlePageClick}
-                            pageRangeDisplayed={3}
-                            marginPagesDisplayed={2}
-                            pageCount={totalPages}
-                            previousLabel="< previous"
-                            pageClassName="page-item"
-                            pageLinkClassName="page-link"
-                            previousClassName="page-item"
-                            previousLinkClassName="page-link"
-                            nextClassName="page-item"
-                            nextLinkClassName="page-link"
-                            breakLabel="..."
-                            breakClassName="page-item"
-                            breakLinkClassName="page-link"
-                            containerClassName="pagination"
-                            activeClassName="active"
-                            renderOnZeroPageCount={null}
-                        />
+                                                        >Delete</button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </>
+                                    :
+                                    <>
+                                        <tr><td>Not found User</td></tr>
+                                    </>}
+                            </tbody>
+                        </table>
+
                     </div>
-                }
+                    {totalPages > 0 &&
+                        <div className="user-footer">
+                            <ReactPaginate
+                                nextLabel="next >"
+                                onPageChange={handlePageClick}
+                                pageRangeDisplayed={3}
+                                marginPagesDisplayed={2}
+                                pageCount={totalPages}
+                                previousLabel="< previous"
+                                pageClassName="page-item"
+                                pageLinkClassName="page-link"
+                                previousClassName="page-item"
+                                previousLinkClassName="page-link"
+                                nextClassName="page-item"
+                                nextLinkClassName="page-link"
+                                breakLabel="..."
+                                breakClassName="page-item"
+                                breakLinkClassName="page-link"
+                                containerClassName="pagination"
+                                activeClassName="active"
+                                renderOnZeroPageCount={null}
+                            />
+                        </div>
+                    }
+                </div>
             </div>
-        </div>
-
+            <ModalDelete
+                show={isShowModalDelete}
+                handleClose={handleClose}
+                confirmDeleteUser={confirmDeleteUser}
+                dataModal={dataModal}
+            />
+        </>
     )
 }
 
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -14,8 +14,11 @@ const loginUser = (valueLogin, password) => {
 const fetchAllUser = (page, limit) => {
     return axios.get(`http://localhost:8080/api/v1/user/read?page=${page}&limit=${limit}`);//template String
 }
+const deleteUser = (user) => {
+    return axios.delete('http://localhost:8080/api/v1/user/delete', { data: { id: user.id } })
+}
 
 
 export {
-    registerNewUser, loginUser, fetchAllUser
+    registerNewUser, loginUser, fetchAllUser, deleteUser
 }
